1edf607a0f237bb7ae59423312904031d155339f
HBI-Developer <www.husam709@gmail.com>
2022-10-08 21:46:44 +0200

    Update Check Page Component

--- src/Components/CheckPage/CheckPage.js
+++ src/Components/CheckPage/CheckPage.js
@@ -9,21 +9,21 @@ export default function CheckPage() {
     if (startPages.indexOf(window.location.pathname) !== -1) {
       if (sessionStorage.getItem('RRS_username') !== null) {
         if (sessionStorage.getItem('RRS_role') === 'admin') {
-          navigate('/admin-page');
+          navigate('/react-results-system/admin-page');
         } else {
-          navigate('/student-page');
+          navigate('/react-results-system/student-page');
         }
       }
     } else {
       if (sessionStorage.getItem('RRS_username') === null) {
-        navigate('/')
+        navigate('/react-results-system/')
       } else {
         if ((window.location.pathname === '/student-page' || window.location.pathname === '/certificate') && sessionStorage.getItem('RRS_role') === 'admin') {
-          navigate('/admin-page');
+          navigate('/react-results-system/admin-page');
         }
 
         if ((window.location.pathname !== '/student-page' && window.location.pathname !== '/certificate') && sessionStorage.getItem('RRS_role') === 'student') {
-          navigate('/student-page');
+          navigate('/react-results-system/student-page');
         }
       }
     }
